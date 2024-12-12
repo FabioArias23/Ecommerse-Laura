92a6ca9e2b0c47a888c47eba0f8bb4cc9013c367
Fabio Arias <83869719+FabioArias23@users.noreply.github.com>
2024-12-11 22:48:25 -0300

    primer commit

---
+++ src/app/products/features/product-shell/product.route.ts
@@ -0,0 +1,12 @@
+import {Routes} from '@angular/router'
+
+export default [
+ {
+  path:'products', loadComponent: ()=> import('../product-list/product-list.component'),
+
+ },
+ {
+  path:'**',
+  redirectTo: 'products',
+ }
+]as Routes;
